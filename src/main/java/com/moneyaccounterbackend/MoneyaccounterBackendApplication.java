9a915e26e30243d703ee99399dd40eb12dcd0590
Tibi <tothtibi30@gmail.com>
2021-02-02 18:59:52 +0100

    Initializing the gh repo and uploading the fresh Spring Boot project

---
+++ src/main/java/com/moneyaccounterbackend/MoneyaccounterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moneyaccounterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyaccounterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyaccounterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moneyaccounterbackend/MoneyaccounterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneyaccounterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyaccounterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
