dc17b1ddaab869f62e2cd2347ece45431dcd167f
Tibi <tothtibi30@gmail.com>
2021-02-03 20:34:18 +0100

    UserController returns User object, added PostMapping for new records, added GetMapping to retrieve all records

--- src/main/java/com/moneyaccounterbackend/controller/AccounterController.java
+++ src/main/java/com/moneyaccounterbackend/controller/AccounterController.java
@@ -1,5 +1,6 @@
 package com.moneyaccounterbackend.controller;
 
+import com.moneyaccounterbackend.entity.Record;
 import com.moneyaccounterbackend.service.UserService;
 import com.moneyaccounterbackend.entity.User;
 import org.slf4j.Logger;
@@ -22,25 +23,45 @@ public class AccounterController {
     }
 
     @PostMapping("/login")
-    public boolean loginUser(@RequestParam String username,
+    public User loginUser(@RequestParam String username,
                              @RequestParam String password) {
         log.info("NEW LOGIN: {} with password {}", username, password);
-        return userService.logInUser(username, password);
+        User loggedInUser = userService.logInUser(username, password);
+
+        return loggedInUser;
     }
 
     @PostMapping("/register")
-    public boolean registerUser(@RequestBody User user) {
+    public User registerUser(@RequestBody User user) {
         log.info("NEW REGISTRATION: {}", user);
         User newUser = userService.registerUser(user);
 
         log.debug("USER REGISTERED: {}", newUser);
-        return true;
+        return newUser;
+    }
+
+    @PostMapping("/record/{id}")
+    public boolean addNewRecord(@PathVariable (name = "id") Long id, @RequestBody Record record) {
+        log.info("REQUEST BY USER {}, TO ADD NEW RECORD {}", id, record);
+        boolean successfulAddition = userService.addingNewRecord(id, record);
+
+        log.debug("REQUEST WAS SUCCESSFUL {}", successfulAddition);
+        return successfulAddition;
     }
 
     @GetMapping("/list")
     public List<User> listUsers() {
         log.info("RECEIVED REQUEST TO LIST ALL USERS.");
         return userService.listAllUsers();
+    }
+
+    @GetMapping("/records/{id}")
+    public List<Record> getAllRecords(@PathVariable (name = "id") Long id) {
+        log.info("REQUEST FOR RECORDS BY ID {}", id);
+        List<Record> listOfRecordsFound = userService.retrieveRecords(id);
+
+        log.debug("REQUEST WAS SUCCESSFUL: {}", listOfRecordsFound);
+        return listOfRecordsFound;
     }
 
 }
