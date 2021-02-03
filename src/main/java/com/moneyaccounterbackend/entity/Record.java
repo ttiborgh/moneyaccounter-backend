6b7228ac466e131c06119823c29c277ecb163388
Tibi <tothtibi30@gmail.com>
2021-02-03 18:45:39 +0100

    Created entity named Record with OneToMany relation to User

---
+++ src/main/java/com/moneyaccounterbackend/entity/Record.java
@@ -0,0 +1,24 @@
+package com.moneyaccounterbackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Record {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String description;
+
+    @Column
+    private String price;
+}
