e99847211bb7f65fa896eeb959b0f8be92f432d4
Tibi <tothtibi30@gmail.com>
2021-04-02 23:07:59 +0200

    Added findByStockName to StockRepository

--- src/main/java/com/moneyaccounterbackend/repository/StockRepository.java
+++ src/main/java/com/moneyaccounterbackend/repository/StockRepository.java
@@ -4,6 +4,9 @@ import com.moneyaccounterbackend.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    Optional<Stock> findByStockName(String stockName);
 }
