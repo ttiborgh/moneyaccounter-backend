9cf1a37c2704d629f8bd0972e4058fd9dede06f1
Tibi <tothtibi30@gmail.com>
2021-04-03 00:02:36 +0200

    Created Exception for invalid Stock data format

---
+++ src/main/java/com/moneyaccounterbackend/exception/InvalidStockFormatException.java
@@ -0,0 +1,8 @@
+package com.moneyaccounterbackend.exception;
+
+public class InvalidStockFormatException extends Exception {
+
+    public InvalidStockFormatException(String message) {
+        super(message);
+    }
+}
