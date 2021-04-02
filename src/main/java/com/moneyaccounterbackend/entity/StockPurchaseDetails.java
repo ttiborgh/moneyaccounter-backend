eca6c249245af2c28fc2058399defb53ce9e4959
Tibi <tothtibi30@gmail.com>
2021-04-02 22:57:03 +0200

    Added proper date format LocalDateTime to StockPurchaseDetails

--- src/main/java/com/moneyaccounterbackend/entity/StockPurchaseDetails.java
+++ src/main/java/com/moneyaccounterbackend/entity/StockPurchaseDetails.java
@@ -1,23 +1,27 @@
 package com.moneyaccounterbackend.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class StockPurchaseDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column
-    private String date;        // TODO: date format later to be added
+    @Column(name = "created_at")
+    @JsonProperty("created_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
     @Column
     private double price;
     @Column
