484ed6edb22e05a2619b1a76fbc4ea8b9cf50b76
Tibi <tothtibi30@gmail.com>
2021-04-02 23:20:23 +0200

    Added new created_at field of type LocalDateTime to Record

--- src/main/java/com/moneyaccounterbackend/entity/Record.java
+++ src/main/java/com/moneyaccounterbackend/entity/Record.java
@@ -1,11 +1,14 @@
 package com.moneyaccounterbackend.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -20,6 +23,10 @@ public class Record {
     private String description;
     @Column
     private Long amount;
+    @Column(name = "created_at")
+    @JsonProperty("created_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
     @Column
     private Boolean spending;
     @ManyToOne(fetch = FetchType.LAZY)
