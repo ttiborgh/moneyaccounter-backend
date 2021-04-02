ad960222bb63585eb983d497b8c3c234de7faf31
Tibi <tothtibi30@gmail.com>
2021-04-02 13:24:46 +0200

    Added StockRepository instance to StockService with Dependency Injection

--- src/main/java/com/moneyaccounterbackend/service/StockService.java
+++ src/main/java/com/moneyaccounterbackend/service/StockService.java
@@ -1,7 +1,16 @@
 package com.moneyaccounterbackend.service;
 
+import com.moneyaccounterbackend.entity.Stock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StockService {
+
+    private final StockRepository stockRepository;
+
+    @Autowired
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
 }
