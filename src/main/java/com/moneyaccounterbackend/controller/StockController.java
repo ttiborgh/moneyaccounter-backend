697c15ff6ef590d8f5092f5ab109fee9dc13ba6f
Tibi <tothtibi30@gmail.com>
2021-04-02 13:10:12 +0200

    Dependency injected StockService into StockController

--- src/main/java/com/moneyaccounterbackend/controller/StockController.java
+++ src/main/java/com/moneyaccounterbackend/controller/StockController.java
@@ -1,12 +1,19 @@
 package com.moneyaccounterbackend.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.moneyaccounterbackend.entity.Stock;
+import com.moneyaccounterbackend.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/stocks")
 public class StockController {
 
-    
+    private StockService stockService;
+
+    @Autowired
+    public StockController (StockService stockService) {
+        this.stockService = stockService;
+    }
+
 }
