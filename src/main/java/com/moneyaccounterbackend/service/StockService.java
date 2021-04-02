d632372aa33f085984619fb519a74c0cd699e5c3
Tibi <tothtibi30@gmail.com>
2021-04-02 13:26:26 +0200

    Added SLF4J logger to StockService

--- src/main/java/com/moneyaccounterbackend/service/StockService.java
+++ src/main/java/com/moneyaccounterbackend/service/StockService.java
@@ -1,12 +1,16 @@
 package com.moneyaccounterbackend.service;
 
+import com.moneyaccounterbackend.controller.StockController;
 import com.moneyaccounterbackend.entity.Stock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StockService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StockService.class);
     private final StockRepository stockRepository;
 
     @Autowired
