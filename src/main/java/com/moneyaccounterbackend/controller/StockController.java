4dcff588989b2bd9b92c5b02cce5f7ef8a1f6062
Tibi <tothtibi30@gmail.com>
2021-04-02 13:03:56 +0200

    Created new StockController component for Stock entity's REST API calls

---
+++ src/main/java/com/moneyaccounterbackend/controller/StockController.java
@@ -0,0 +1,12 @@
+package com.moneyaccounterbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stocks")
+public class StockController {
+
+    
+}
