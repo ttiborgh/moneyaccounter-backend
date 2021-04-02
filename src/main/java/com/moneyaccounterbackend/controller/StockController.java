41faffec447d60d69034e593b695db880727e794
Tibi <tothtibi30@gmail.com>
2021-04-02 13:12:51 +0200

    Initialized SLF4J logger in StockController

--- src/main/java/com/moneyaccounterbackend/controller/StockController.java
+++ src/main/java/com/moneyaccounterbackend/controller/StockController.java
@@ -2,6 +2,8 @@ package com.moneyaccounterbackend.controller;
 
 import com.moneyaccounterbackend.entity.Stock;
 import com.moneyaccounterbackend.service.StockService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +12,11 @@ import org.springframework.web.bind.annotation.*;
 public class StockController {
 
     private StockService stockService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(StockController.class);
 
     @Autowired
     public StockController (StockService stockService) {
         this.stockService = stockService;
     }
-
+    
 }
