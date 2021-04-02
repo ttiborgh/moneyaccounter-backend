09fc00b3292210f3bbd912132b4a35aed271d621
Tibi <tothtibi30@gmail.com>
2021-04-02 14:02:22 +0200

    Optimized imports in StockService

--- src/main/java/com/moneyaccounterbackend/service/StockService.java
+++ src/main/java/com/moneyaccounterbackend/service/StockService.java
@@ -1,6 +1,5 @@
 package com.moneyaccounterbackend.service;
 
-import com.moneyaccounterbackend.controller.StockController;
 import com.moneyaccounterbackend.entity.Stock;
 import com.moneyaccounterbackend.entity.User;
 import com.moneyaccounterbackend.repository.StockRepository;
@@ -11,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Optional;
 
 @Service
 public class StockService {
