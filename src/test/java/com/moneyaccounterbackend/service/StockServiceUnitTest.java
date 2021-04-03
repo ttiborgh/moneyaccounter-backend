2ed1de13b627eb038fddc33a4f8d492a30e300c4
Tibi <tothtibi30@gmail.com>
2021-04-03 12:38:55 +0200

    Added happy case test when creating whole new stock

--- src/test/java/com/moneyaccounterbackend/service/StockServiceUnitTest.java
+++ src/test/java/com/moneyaccounterbackend/service/StockServiceUnitTest.java
@@ -6,6 +6,7 @@ import com.moneyaccounterbackend.entity.User;
 import com.moneyaccounterbackend.exception.InvalidFormatException;
 import com.moneyaccounterbackend.repository.StockRepository;
 import com.moneyaccounterbackend.repository.UserRepository;
+import org.apache.tomcat.jni.Local;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,6 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -47,5 +49,71 @@ class StockServiceUnitTest {
     public void givenNonExistingUserId_whenCreatingNewStock_thenExceptionIsThrown() {
         when(userRepository.findById(USER_ID)).thenThrow(ResponseStatusException.class);
         assertThrows(ResponseStatusException.class, () -> stockService.createNewStockEntry(USER_ID, STOCK_NAME, STOCK_QUANTITY, STOCK_PRICE));
+    }
+
+    @Test
+    public void givenValidData_whenCreatingWholeNewStock_thenStockIsSaved() throws InvalidFormatException {
+        when(userRepository.findById(USER_ID)).thenReturn(Optional.of(givenValidUser()));
+        when(stockRepository.findByStockName(any(String.class))).thenReturn(Optional.empty());
+        when(stockRepository.save(any(Stock.class))).thenAnswer(invocationOnMock -> {
+            Stock stock = invocationOnMock.getArgument(0);                          // TODO: Find another solution later if there is
+            stock.setId(STOCK_ID);
+            stock.getStockPurchaseDetails().get(0).setId(STOCK_PURCHASE_ID);
+            return stock;
+        });
+
+        Stock createdStock = stockService.createNewStockEntry(USER_ID, STOCK_NAME, STOCK_QUANTITY, STOCK_PRICE);
+
+        assertThat(createdStock, notNullValue());
+        assertThat(createdStock.getId(), is(STOCK_ID));
+        assertThat(createdStock.getUser().getId(), is(USER_ID));
+        assertThat(createdStock.getStockName(), is(STOCK_NAME));
+        assertThat(createdStock.getStockPurchaseDetails(), notNullValue());
+        assertThat(createdStock.getStockPurchaseDetails().get(0).getId(), is(STOCK_PURCHASE_ID));
+        assertThat(createdStock.getStockPurchaseDetails().get(0).getStock().getId(), is(STOCK_ID));
+        assertThat(createdStock.getStockPurchaseDetails().get(0).getPrice(), is(STOCK_PRICE));
+        assertThat(createdStock.getStockPurchaseDetails().get(0).getQuantity(), is(STOCK_QUANTITY));
+        assertThat(createdStock.getStockPurchaseDetails().get(0).getCreatedAt(), notNullValue());
+
+        verify(userRepository, times(1)).findById(USER_ID);
+    }
+
+    private User givenValidUser() {
+        return User.builder()
+                .id(USER_ID)
+                .username("User")
+                .balance(0L)
+                .email("valid@email.com")
+                .password("valid")
+                .build();
+    }
+
+    private Stock givenValidStock() {
+        return Stock.builder()
+                .id(STOCK_ID)
+                .user(givenValidUser())
+                .stockName(STOCK_NAME)
+                .stockPurchaseDetails(givenListOfPurchaseDetails())
+                .build();
+    }
+
+    private List<StockPurchaseDetails> givenListOfPurchaseDetails() {
+        return List.of(StockPurchaseDetails.builder()
+                .id(STOCK_PURCHASE_ID)
+                .createdAt(CREATED_AT)
+                .price(STOCK_PRICE)
+                .quantity(STOCK_QUANTITY)
+                .stock(givenValidStock())
+                .build());
+    }
+
+    private StockPurchaseDetails givenPurchaseDetails() {
+        return StockPurchaseDetails.builder()
+                .id(STOCK_PURCHASE_ID)
+                .createdAt(CREATED_AT)
+                .price(STOCK_PRICE)
+                .quantity(STOCK_QUANTITY)
+                .stock(givenValidStock())
+                .build();
     }
 }
