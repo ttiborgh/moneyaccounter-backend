d9b25d0dd87482177e0100b3764814ec8acb864b
Tibi <tothtibi30@gmail.com>
2021-04-03 12:32:19 +0200

    Added Builder annotation to User entity to make initialisation easier

--- src/main/java/com/moneyaccounterbackend/entity/User.java
+++ src/main/java/com/moneyaccounterbackend/entity/User.java
@@ -3,6 +3,7 @@ package com.moneyaccounterbackend.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
