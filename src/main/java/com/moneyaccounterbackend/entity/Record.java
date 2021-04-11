8449190e3625b8f1489ecb50587a526186101695
Tibi <tothtibi30@gmail.com>
2021-04-11 20:26:36 +0200

    Added Builder annotation to Record entity

--- src/main/java/com/moneyaccounterbackend/entity/Record.java
+++ src/main/java/com/moneyaccounterbackend/entity/Record.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
