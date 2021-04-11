41766d71cd0692a6577e9147afa9d4d08e060098
Tibi <tothtibi30@gmail.com>
2021-04-11 20:26:16 +0200

    Added constant values to represent a Record's fields

--- src/test/java/com/moneyaccounterbackend/service/RecordServiceTest.java
+++ src/test/java/com/moneyaccounterbackend/service/RecordServiceTest.java
@@ -1,16 +1,27 @@
 package com.moneyaccounterbackend.service;
 
+import com.moneyaccounterbackend.entity.Record;
 import com.moneyaccounterbackend.repository.RecordRepository;
 import com.moneyaccounterbackend.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.time.Month;
 
 @ExtendWith(MockitoExtension.class)
 class RecordServiceTest {
+
+    private static final long RECORD_ID = 1L;
+    private static final long USER_ID = 2L;
+    private static final String DESCRIPTION = "Travelling expenses";
+    private static final long AMOUNT = 500L;
+    private static final LocalDateTime CREATED_AT = LocalDateTime.of(2021,
+            Month.APRIL, 11, 20, 23, 59);
+    private static final boolean SPENDING = true;
 
     @Mock
     private RecordRepository recordRepository;
@@ -18,4 +29,5 @@ class RecordServiceTest {
     private UserRepository userRepository;
     @InjectMocks
     private RecordService recordService;
+
 }
