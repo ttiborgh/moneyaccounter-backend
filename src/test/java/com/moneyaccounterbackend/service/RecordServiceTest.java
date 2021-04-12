f505b531533afc0ba35c9e9814988e8527f55e40
Tibi <tothtibi30@gmail.com>
2021-04-12 11:54:37 +0200

    Added happy path test case when creating new Record

--- src/test/java/com/moneyaccounterbackend/service/RecordServiceTest.java
+++ src/test/java/com/moneyaccounterbackend/service/RecordServiceTest.java
@@ -1,6 +1,8 @@
 package com.moneyaccounterbackend.service;
 
 import com.moneyaccounterbackend.entity.Record;
+import com.moneyaccounterbackend.entity.Stock;
+import com.moneyaccounterbackend.entity.User;
 import com.moneyaccounterbackend.repository.RecordRepository;
 import com.moneyaccounterbackend.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNotNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class RecordServiceTest {
@@ -30,4 +42,49 @@ class RecordServiceTest {
     @InjectMocks
     private RecordService recordService;
 
+    @Test
+    public void givenValidUserIdAndRecordData_whenAddingNewRecordByUser_thenRecordIsCreated() {
+        givenMockedUserRepositoryFindingUser();
+        when(recordRepository.save(any(Record.class))).thenAnswer(invocationOnMock -> {
+            Record record = invocationOnMock.getArgument(0);
+            record.setId(RECORD_ID);
+            return record;
+        });
+
+        Record record = recordService.addingNewRecord(USER_ID, givenValidRecordDTO());
+
+        assertThat(record.getId(), is(RECORD_ID));
+        assertThat(record.getUser().getId(), is(USER_ID));
+        assertThat(record.getAmount(), is(AMOUNT));
+        assertThat(record.getDescription(), is(DESCRIPTION));
+        assertThat(record.getCreatedAt(), notNullValue());
+        assertThat(record.getSpending(), is(SPENDING));
+
+        verify(recordRepository, times(1)).save(any(Record.class));
+        verify(userRepository, times(1)).findById(USER_ID);
+    }
+
+    private User givenExistingUserWithoutRecords() {
+        return User.builder()
+                .id(USER_ID)
+                .username("User")
+                .balance(0L)
+                .email("valid@email.com")
+                .password("valid")
+                .listOfRecords(new ArrayList<>())
+                .build();
+    }
+
+    private Record givenValidRecordDTO() {
+        return Record.builder()
+                .amount(AMOUNT)
+                .spending(SPENDING)
+                .description(DESCRIPTION)
+                .createdAt(CREATED_AT)
+                .build();
+    }
+
+    private void givenMockedUserRepositoryFindingUser() {
+        when(userRepository.findById(USER_ID)).thenReturn(Optional.of(givenExistingUserWithoutRecords()));
+    }
 }
